b59774686852e60ac6bdb2b2462a5b39dd1af183
Phew <eurojkryns@gmail.com>
2023-12-08 21:13:34 +0700

    score

---
+++ Exercise4_Jakrin_Y.py
@@ -0,0 +1,11 @@
+FE = 87
+GB = 90
+IC = 91.5
+CP = 84
+print("---------------------------------")
+print("SCORE")
+print("---------------------------------")
+print("Foundation English       :", FE)
+print("General Business         :", GB)
+print("Introduction to Computer :", IC)
+print("Computer Programming     :", CP)
