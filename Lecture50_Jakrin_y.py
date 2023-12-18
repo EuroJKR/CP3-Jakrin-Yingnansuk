60af579c1a3ff794407fa9324ed46e4a9151464a
EuroJKR <153280831+EuroJKR@users.noreply.github.com>
2023-12-18 17:52:33 +0700

    Create Lecture50_Jakrin_y.py

---
+++ Lecture50_Jakrin_y.py
@@ -0,0 +1,11 @@
+def Calculate(x,y):
+    print("Result : ")
+    print(x,"+",y,"=",x+y)
+    print(x,"-",y,"=",x-y)
+    print(x,"*",y,"=",x*y)
+    print(x,"/",y,"=",x/y)
+
+x = int(input("First Number : "))
+y = int(input("Second Number : "))
+
+Calculate(x,y)
